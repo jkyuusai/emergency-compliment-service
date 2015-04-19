eb4a1c119da37585da3b6535a4f06bd65a7c15ad
Joshua Skinner <jkyuusai@gmail.com>
2015-04-19 18:36:40 -0400

    Scrape compliment from site using capybara/poltergeist and set it in response

--- service.rb
+++ service.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'capybara'
+require 'capybara/poltergeist'
+
+include Capybara::DSL
+Capybara.default_driver = :poltergeist
 
 set :server, 'thin'
+set :protection, :except => :json_csrf
 
-get '/' do	
-	json :compliment => 'a compliment', :missing_words => []
+get '/' do
+	json get_compliment
+end
+
+def get_compliment
+	visit 'http://emergencycompliment.com/'
+	compliment = find('.compliment').text
+	{:compliment => compliment, :missing_words => []}
 end
